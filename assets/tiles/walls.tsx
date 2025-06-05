d54248856b2ffa1ab2a6631ef089fc47115b7750
IndianaOrz <indianaorz@gmail.com>
2025-06-04 22:25:59 -0400

    working on lab

--- assets/tiles/walls.tsx
+++ assets/tiles/walls.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="walls" tilewidth="64" tileheight="109" tilecount="1" columns="1">
- <tileoffset x="0" y="64"/>
+<tileset version="1.10" tiledversion="1.11.2" name="walls" tilewidth="64" tileheight="109" tilecount="2" columns="2" objectalignment="bottom">
+ <tileoffset x="0" y="20"/>
  <grid orientation="orthogonal" width="62" height="109"/>
- <image source="walls.png" width="64" height="109"/>
+ <image source="walls.png" width="128" height="109"/>
 </tileset>
